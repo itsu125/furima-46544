2fd552e17b0c99baaaa9d384d922e659e610d11a
itsu125 <iikura.20250908@gmail.com>
2025-10-17 02:24:03 +0900

    画像保存(ActiveStorage)の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,4 +15,8 @@ class Item < ApplicationRecord
                       greater_than_or_equal_to: 300,
                       less_than_or_equal_to: 9_999_999,
                     }
+
+  has_one_attached :image
+  validates :image, presence: true
+
 end
