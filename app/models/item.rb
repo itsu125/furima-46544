001284500a0e19bfabb68727229a491ce95ab186
itsu125 <iikura.20250908@gmail.com>
2025-10-17 00:50:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :name, :description, :category_id, :condition_id, 
+  :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true
+  validates :price, presence: true,
+                    numericality: {
+                      only_integer: true,
+                      greater_than_or_equal_to: 300,
+                      less_than_or_equal_to: 9_999_999,
+                    }
 end
