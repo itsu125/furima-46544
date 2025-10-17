aa13a0fdc769773f35aaf3d6b1a21f5aa9ca3dc4
itsu125 <iikura.20250908@gmail.com>
2025-10-17 15:43:10 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+
+  has_many :items, dependent: :destroy
 end
