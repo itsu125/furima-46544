09549cbcfbd0e4ab01cae0b0e1efaa186c53ee3a
itsu125 <iikura.20250908@gmail.com>
2025-10-20 20:53:54 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   def move_to_index
     @item = Item.find(params[:id])
     return if current_user == @item.user
+
     redirect_to root_path
   end
-
 end
