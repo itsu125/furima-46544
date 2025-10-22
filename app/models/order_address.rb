adc34da6b6d62a20603459add8b908044d447a52
itsu125 <iikura.20250908@gmail.com>
2025-10-23 00:07:00 +0900

    エラーメッセージの表示と修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,31 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :street_address, :token
-    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :city
+    validates :street_address
+    validates :post_code, format: {
+      with: /\A\d{3}-\d{4}\z/,
+      message: "is invalid. Enter it as follows (e.g. 123-4567)"
+    }
+    validates :phone_number, format: {
+      with: /\A\d{10,11}\z/,
+      message: "is invalid. Input only number"
+    }
   end
+
+  validates :prefecture_id, numericality: {
+    other_than: 1,
+    message: "can't be blank"
+  }
+
+  validates :phone_number, length: {
+    minimum: 10,
+    maximum: 11,
+    message: "is too short"
+  }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
