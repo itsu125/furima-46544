72802917114ae070396ec4bfbcf9c98a9f29af77
itsu125 <iikura.20250908@gmail.com>
2025-10-22 18:27:30 +0900

    コントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,36 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_root
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end
+  end
 end
